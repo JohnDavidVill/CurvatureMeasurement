7be8d09c8b99045e88ad52e208d4a14efca4524b
JohnDavidVill <107506620+JohnDavidVill@users.noreply.github.com>
2023-05-24 22:25:11 -0500

    Update hello.c

--- UART_dummy_data/hello.c
+++ UART_dummy_data/hello.c
@@ -5,22 +5,26 @@
  * This code is simply a UART example and shows how the MSP430 will implement the sensor readings
  * This code is meant to test the code for MatLab found in the repository
  *
- * This code simply counts down from 255 to 0, repeating itself indefinitely. An arbitrary
- * amount of cycles are used for the break in between printing
+ * This code simply counts down from 255 to 0 
  *
  */
 #include <stdio.h>
 
 void main() {
 
-	while(1) {
+	for(int i = 255; i > 0; i--) {
 
-		for(int i = 255; i > 0; i--) {
+		printf("A%d\r\n", i);
+		for(int j = 0; j <= 1000000; j++);
 
-			printf("A%d\r\n", i);
-			for(int j = 0; j <= 1000000; j++);
+	}
 
-		}
+	for(int j = 0; j <= 10000000; j++);
+
+	for(int i = 255; i > 0; i--) {
+
+		printf("B%d\r\n", i);
+		for(int j = 0; j <= 1000000; j++);
 
 	}
 
