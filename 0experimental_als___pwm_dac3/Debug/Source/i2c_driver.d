f4eb1c89c185ae6704b06052a485096b93559b2c
JackSPearce <89664692+JackSPearce@users.noreply.github.com>
2023-08-23 15:34:57 -0500

    Fixed 32-bit bug
    Possible bug due to use of 32-bit uint in the exponent portion of lux calculation has been fixed.

---
+++ 0experimental_als___pwm_dac3/Debug/Source/i2c_driver.d
@@ -0,0 +1,57 @@
+# FIXED
+
+Source/i2c_driver.obj: ../Source/i2c_driver.c
+Source/i2c_driver.obj: ../Source/i2c_driver.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430fr2433.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h
+Source/i2c_driver.obj: C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h
+
+../Source/i2c_driver.c:
+
+../Source/i2c_driver.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/msp430fr2433.h:
+
+C:/ti/ccs1220/ccs/ccs_base/msp430/include/in430.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/intrinsics_legacy_undefs.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_ti_config.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/linkage.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/_stdint40.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_types.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_types.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1220/ccs/tools/compiler/ti-cgt-msp430_21.6.1.LTS/include/stdbool.h:
+
